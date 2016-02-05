efd5698551242591fb316aa9d6f0aa839bd2da29
OpenStack Developer <example@example.com>
2016-02-05 16:20:13 +1100

    Add latest changes.
    Currently stymied because AuthKit has borkened dependencies

--- Vagrantfile
+++ Vagrantfile
@@ -72,6 +72,8 @@ Vagrant.configure(2) do |config|
 
   config.vm.provision "ansible_local" do |ansible|
     ansible.playbook = "playbook.yml"
+    ansible.install  = true
+    ansible.verbose  = true
   end
   
 end
--- configure.sh
+++ configure.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 ZOOKEEPR_REPO=${ZOOKEEPR_REPO:-git@github.com:jamezpolley/zookeepr.git}
+PIL_SRC=${PIL_SRC:-http://effbot.org/downloads/Imaging-1.1.7.tar.gz}
 
 mkdir -p src
 if [ ! -e src/zookeepr ]; then
     git clone git@github.com:jamezpolley/zookeepr.git src/zookeepr
 fi
+
+if [ ! -e src/Imaging-1.1.7.tar.gz ]; then
+    pushd src
+    curl -L -O http://effbot.org/downloads/Imaging-1.1.7.tar.gz
+    popd
+fi
+
+virtualenv .
+source bin/activate && pip install -r requirements.txt
+
